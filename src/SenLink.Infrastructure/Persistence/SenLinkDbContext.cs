dbea603d51b9e6993150a935b7a20a2712fb50eb
torakiti-1128 <umeda_hiromu@outlook.jp>
2026-03-01 22:57:17 +0900

    feat(Infra): DbContextにSchoolServiceのEntityを追加(#9)

--- src/SenLink.Infrastructure/Persistence/SenLinkDbContext.cs
+++ src/SenLink.Infrastructure/Persistence/SenLinkDbContext.cs
@@ -1,5 +1,7 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using SenLink.Domain.Modules.Auth.Entities;
+using SenLink.Domain.Modules.School.Entities;
 
 namespace SenLink.Infrastructure.Persistence;
 
@@ -8,17 +10,30 @@ namespace SenLink.Infrastructure.Persistence;
 /// </summary>
 public class SenLinkDbContext : DbContext
 {
-    public SenLinkDbContext(DbContextOptions<SenLinkDbContext> options) : base(options) { }
+    public SenLinkDbContext(DbContextOptions<SenLinkDbContext> options)
+        : base(options)
+    {
+    }
 
-    // 各モジュールのDbSet
+    // Auth Service Entities
     public DbSet<Account> Accounts => Set<Account>();
     public DbSet<LoginHistory> LoginHistories => Set<LoginHistory>();
 
+    // School Service Entities
+    public DbSet<Department> Departments => Set<Department>();
+    public DbSet<Class> Classes => Set<Class>();
+    public DbSet<Teacher> Teachers => Set<Teacher>();
+    public DbSet<Student> Students => Set<Student>();
+    public DbSet<ClassTeacher> ClassTeachers => Set<ClassTeacher>();
+
+    /// <summary>
+    /// モデルを構成する
+    /// IEntityTypeConfiguration を実装したクラスを自動で読み込むようにする
+    /// </summary>
+    /// <param name="modelBuilder"></param>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // Infrastructure 内の全 IEntityTypeConfiguration を自動適用
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SenLinkDbContext).Assembly);
-        
         base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
